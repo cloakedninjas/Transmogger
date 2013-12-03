ec4de2dee1f9827496534e907603e129356ed109
cloakedninjas <danyboyj@gmail.com>
2013-12-03 16:43:18 +0000

    Stubs

--- public_html/js/main.js
+++ public_html/js/main.js
@@ -1,5 +1,58 @@
 var BaseView = Backbone.View.extend({
-  initalize: function () {
-    console.log(1);
+  render: function() {
+    this.$el.html(this.template());
+    return this;
+  }
+});
+
+var App = BaseView.extend({
+  className: 'app',
+  
+  _modelViewer = null,
+  _optionsPanel = null,
+  
+  initialize: function () {
+  
+  }
+});
+
+var ModelView = BaseView.extend({
+  className: 'model-viewer',
+  template: '<flash></flash>'
+});
+
+var OptionsPanelView = BaseView.extend({
+  className: 'options-panel',
+});
+
+var LoadoutModel = Backbone.Model.extend({
+  urlRoot: '/api/loadout'
+});
+
+var ItemModel = Backbone.Model.extend({
+  urlRoot: '/api/item',
+  
+});
+
+var ItemCollection = Backbone.Collection.extend({
+  model: ItemModel,
+  url: '/api/item'
+  
+  
+  /*
+  initialize: function (models, options) {
+    this._query = options.query;
+  },
+  
+  url: function () {
+    return '/api/search/item?q=' + encodeURIComponent(this._query);
+  }*/
+  
+  search: function (query) {
+    this.fetch({
+      data: {
+        query: query
+      }
+    });
   }
 });
